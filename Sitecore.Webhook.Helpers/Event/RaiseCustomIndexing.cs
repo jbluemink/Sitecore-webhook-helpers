54b64156db30a353a55f1a0d7c05a08e6a744d33
Jan Bluemink <jbluemink@uxbee.nl>
2022-12-22 17:08:37 +0100

    Add custom event

---
+++ Sitecore.Webhook.Helpers/Event/RaiseCustomIndexing.cs
@@ -0,0 +1,45 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Events;
+using Sitecore.Extensions;
+using Sitecore.Webhook.Helpers.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Webhook.Helpers.Event
+{
+    public class RaiseCustomIndexing
+    {
+        public void AddIndexName(object sender, EventArgs args)
+        {
+            if (args != null)
+            {
+
+                SitecoreEventArgs sitecoreEventArgs = args as SitecoreEventArgs;
+                if (sitecoreEventArgs != null && sitecoreEventArgs.Parameters != null && sitecoreEventArgs.Parameters.Length > 0)
+                {
+                    Item eventItem = Context.ContentDatabase.GetItem("/sitecore/system/Settings/Webhooks/Rule Items/" + sitecoreEventArgs.Parameters[0]);
+
+                    if (sitecoreEventArgs.Parameters != null && eventItem != null)
+                    {
+
+                        IList<object> parameters = new List<object>();
+                        parameters.Add(eventItem);
+                       
+
+                        var teamsMessage = new text();
+                        teamsMessage.textmessage = "oo oo ooo";
+                        parameters.Add(teamsMessage);
+
+                        parameters.AddRange(sitecoreEventArgs.Parameters);
+                        object[] objArray = parameters.Cast<object>().ToArray();
+                        Sitecore.Events.Event.RaiseEvent("customindexing:end", objArray);
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Sitecore.Webhook.Helpers/Model/TeamsText.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Sitecore.Webhook.Helpers.Converter;
+
+
+namespace Sitecore.Webhook.Helpers.Model
+{
+    [JsonConverter(typeof(ToStringJsonConverter))]
+    public class text
+    {
+        public string textmessage;
+
+        public override string ToString()
+        {
+            return textmessage;
+        }
+    }
+}
